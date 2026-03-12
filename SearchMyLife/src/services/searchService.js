1aea5eddfa54b15f603c567c5ab941ca60ccd6d3
ColtonKnopik <ColtonKnopik@gmail.com>
2026-03-11 23:06:59 -0700

    Change package name

--- SearchMyLife/src/services/searchService.js
+++
@@ -1,6 +0,0 @@
-import apiClient from '@/services/apiClient'
-
-export async function searchEntries(query) {
-  const response = await apiClient.post('/search', { query })
-  return response.data
-}
